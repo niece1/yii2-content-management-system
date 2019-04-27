2ca12f7b576d62f2906a799384e041dfa0778091
Volodymyr Zhonchuk <vzhonchuk@gmail.com>
2019-04-27 23:54:06 +0300

    Behaviors added

--- modules/admin/Module.php
+++ modules/admin/Module.php
@@ -16,6 +16,27 @@ class Module extends \yii\base\Module
     /**
      * {@inheritdoc}
      */
+    public function behaviors()
+    {
+        return [
+            'access'    =>  [
+                'class' =>  AccessControl::className(),
+                'denyCallback'  =>  function($rule, $action)
+                {
+                    throw new \yii\web\NotFoundHttpException();
+                },
+                'rules' =>  [
+                    [
+                        'allow' =>  true,
+                        'matchCallback' =>  function($rule, $action)
+                        {
+                            return Yii::$app->user->identity->isAdmin;
+                        }
+                    ]
+                ]
+            ]
+        ];
+    }
     public function init()
     {
         parent::init();
--- views/registration/login.php
+++ views/registration/login.php
@@ -1,7 +1,6 @@
 <?php
 
 /* @var $this yii\web\View */
-/* @var $form yii\bootstrap\ActiveForm */
 /* @var $model app\models\LoginForm */
 
 use yii\helpers\Html;
